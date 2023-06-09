6b304b1d77578ffe4833809b853bf51decb95efa
Manh <97645883+Nasa1CODE@users.noreply.github.com>
2023-06-09 15:15:58 +0700

    commit cau truc lenh co ban

---
+++ CauTrucLenhCoBan/CauTrucLenhCoBan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CauTrucLenhCoBan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = { "Man, Xoai, Chuoi, Tao"};
+
+
+            foreach(var items in list)
+            {
+                Console.Write("Hoa qua: " + items);
+            }
+        }
+    }
+}
