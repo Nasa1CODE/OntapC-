8aa342840a48a9b02d9cad10e105ccaff2aa029b
Manh <97645883+Nasa1CODE@users.noreply.github.com>
2023-06-09 15:01:32 +0700

    commit caclenhnhay

---
+++ Caclenhnhay/Caclenhnhay/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Caclenhnhay
+{
+
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+           
+            //break vs continue
+            for(int i = 0; i<= 10; i++)
+            {
+                if( i > 3)
+                {
+                    break;
+                }
+                Console.WriteLine("A["+i+"]=" + i);
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine("A[" + i + "]=" + i);
+            }
+
+
+            //a+b
+            Console.Write("Tong hai so 3 + 7 = " + Add(3,7));
+        }
+    }
+
+}
